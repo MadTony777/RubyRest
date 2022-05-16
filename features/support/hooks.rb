b48ffe38f8ad6200c9ac2edbdcf7ccb43a5bb31f
MadTony777 <42567417+MadTony777@users.noreply.github.com>
2022-05-16 12:12:49 +0300

    delete comment in hooks

--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -6,7 +6,4 @@ end
 After do |scenario|
   default = Default.new
   default.info('----- Test Case finish -----')
-  if scenario.failed?
-    # encoded_img = $driver.screenshot_as(:base64)
-  end
 end
